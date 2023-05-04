525b929689d60133646bc6fb356078aebcf4469a
nikolaospapa3 <nikolaospapa3@gmail.com>
2023-05-05 00:20:37 +0300

    change in validate function and renamed task in db

--- task.sqlite.sql
+++ task.sqlite.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS task;
 DROP TABLE IF EXISTS dev_takes_task;
 
 CREATE TABLE dev_takes_task
-(name varchar(50),
+(task varchar(50),
 email varchar(30),
-primary key (name, email)
+primary key (task, email)
 );
